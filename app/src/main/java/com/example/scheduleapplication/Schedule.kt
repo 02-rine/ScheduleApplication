89432c033ade970018fa0481c1c2e4a5b2a7f4ad
r203323 <kobayashi.reni.ot@tut.jp>
2020-12-23 20:50:12 +0900

    feat: 日付と詳細の属性を追加

--- app/src/main/java/com/example/scheduleapplication/Schedule.kt
+++ app/src/main/java/com/example/scheduleapplication/Schedule.kt
@@ -3,7 +3,11 @@ package com.example.scheduleapplication
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.util.*
 
 @Entity(tableName = "schedule_table")
 data class Schedule(
-    @PrimaryKey @ColumnInfo(name = "title") val title: String) // タイトル
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id") val id: Int, // ID（主キー）
+    @ColumnInfo(name = "date") val date: String, // 日付
+    @ColumnInfo(name = "title") val title: String, // タイトル
+    @ColumnInfo(name = "detail") val detail: String?) // 詳細
