dcf1d866a3a1507dd23ccc744cad906049e550d5
r203323 <kobayashi.reni.ot@tut.jp>
2020-12-25 14:33:37 +0900

    feat: データベースに新たな属性（開始日時・終了日時）の追加

--- app/src/main/java/com/example/scheduleapplication/Schedule.kt
+++ app/src/main/java/com/example/scheduleapplication/Schedule.kt
@@ -3,11 +3,13 @@ package com.example.scheduleapplication
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import java.util.*
 
 @Entity(tableName = "schedule_table")
 data class Schedule(
-    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id") val id: Int, // ID（主キー）
-    @ColumnInfo(name = "date") val date: String, // 日付
-    @ColumnInfo(name = "title") val title: String, // タイトル
-    @ColumnInfo(name = "detail") val detail: String?) // 詳細
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id") val id: Int,
+    @ColumnInfo(name = "title") val title: String?, // タイトル
+    @ColumnInfo(name = "detail") val detail: String?, // 詳細
+    @ColumnInfo(name = "startDate") val startDate: String, // 開始の日付
+    @ColumnInfo(name = "startTime") val startTime: String, // 開始の時刻
+    @ColumnInfo(name = "endDate") val endDate: String, // 終了の日付
+    @ColumnInfo(name = "endTime") val endTime: String) // 終了の時刻
