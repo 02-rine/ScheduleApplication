82e06f6df8c04c87022589250a473b08f342db2b
r203323 <kobayashi.reni.ot@tut.jp>
2020-12-26 14:38:37 +0900

    feat: リポジトリにアクセスするため処理の追加

--- app/src/main/java/com/example/scheduleapplication/ScheduleViewModel.kt
+++ app/src/main/java/com/example/scheduleapplication/ScheduleViewModel.kt
@@ -1,12 +1,12 @@
 package com.example.scheduleapplication
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
+import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.*
+import java.util.*
 import kotlin.coroutines.CoroutineContext
 
 // データの保持・管理をするクラス
@@ -14,27 +14,50 @@ class ScheduleViewModel(application: Application) : AndroidViewModel(application
 
     // 非同期処理をするための宣言
     private var parentJob = Job()
-    private val coroutineContext: CoroutineContext
-        get() = parentJob + Dispatchers.Main
+    private val coroutineContext: CoroutineContext = parentJob + Dispatchers.Main
     private val scope = CoroutineScope(coroutineContext)
 
     private val repository: ScheduleRepository
     val allSchedule: LiveData<List<Schedule>> // データベースの全データを格納
+    val countDate:  LiveData<List<CountDate>> // 日付と日付のカウント数を格納
+    val daySchedule = MutableLiveData<List<Schedule>>() // 選択した日付の全データを格納
+    var idSchedule = MutableLiveData<Schedule>()
 
     init{
         val scheduleDao = ScheduleRoomDatabase.getDatabase(application, scope).scheduleDao()
         repository = ScheduleRepository(scheduleDao)
         allSchedule = repository.allSchedule // データベースの全データを取得
+        countDate = repository.countDate
+    }
+
+    // 選択した日付の全データを開始時刻の昇順で取得
+    fun setDaySchedule(data: String) = scope.launch(Dispatchers.IO){
+        daySchedule.postValue(repository.getDaySchedule(data))
+    }
+
+    // IDに一致するデータを取得
+    fun setIdSchedule(id: Int) = scope.launch(Dispatchers.IO) {
+        idSchedule.postValue(repository.getIDSchedule(id))
+    }
+
+    // データベースへデータを入力
+    fun insert(schedule: Schedule) = scope.launch(Dispatchers.IO) {
+        repository.insert(schedule)
+    }
+
+    // データベースのデータを削除
+    fun delete(schedule: Schedule) = scope.launch(Dispatchers.IO){
+        repository.delete(schedule)
+    }
+
+    // データベースのデータを変更
+    fun update(schedule: Schedule) = scope.launch(Dispatchers.IO){
+        repository.update(schedule)
     }
 
     // parentJobをキャンセル
     override fun onCleared() {
         super.onCleared()
         parentJob.cancel()
-    }
-
-    // データベースへデータを入力する
-    fun insert(schedule: Schedule) = scope.launch(Dispatchers.IO) {
-        repository.insert(schedule)
     }
 }
