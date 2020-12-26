a17b96cf195e2c13817bace0a2a31cb1399d37cd
r203323 <kobayashi.reni.ot@tut.jp>
2020-12-26 14:31:31 +0900

    feat: 日付のカウント数を格納するためのデータクラスを追加

--- app/src/main/java/com/example/scheduleapplication/Schedule.kt
+++ app/src/main/java/com/example/scheduleapplication/Schedule.kt
@@ -5,11 +5,17 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "schedule_table")
+// データベースの属性データを持つクラス
 data class Schedule(
     @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id") val id: Int,
     @ColumnInfo(name = "title") val title: String?, // タイトル
     @ColumnInfo(name = "detail") val detail: String?, // 詳細
-    @ColumnInfo(name = "startDate") val startDate: String, // 開始の日付
+    @ColumnInfo(name = "date") val date: String, // 日付
     @ColumnInfo(name = "startTime") val startTime: String, // 開始の時刻
-    @ColumnInfo(name = "endDate") val endDate: String, // 終了の日付
     @ColumnInfo(name = "endTime") val endTime: String) // 終了の時刻
+
+// データベースの日付と日付のカウンタ数を持つクラス
+data class CountDate(
+        @ColumnInfo(name = "date") val date: String, // 日付
+        @ColumnInfo(name = "count") val count: Int // 日付のカウンタ数
+)
