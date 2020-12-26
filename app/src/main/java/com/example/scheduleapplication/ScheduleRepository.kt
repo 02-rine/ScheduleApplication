fd2190303e3f628b3e5e3d17afac0309815f5861
r203323 <kobayashi.reni.ot@tut.jp>
2020-12-26 14:37:26 +0900

    feat: データベースへアクセスするための処理の追加

--- app/src/main/java/com/example/scheduleapplication/ScheduleRepository.kt
+++ app/src/main/java/com/example/scheduleapplication/ScheduleRepository.kt
@@ -1,17 +1,35 @@
 package com.example.scheduleapplication
 
-import androidx.annotation.WorkerThread
 import androidx.lifecycle.LiveData
 
-// データベースへアクセスリポジトリ
+// データベースへアクセスするリポジトリクラス
 class ScheduleRepository(private val scheduleDao: ScheduleDao){
 
-    // データベースの全データを取得する
-    val allSchedule: LiveData<List<Schedule>> = scheduleDao.getAllSchedule( )
+    val allSchedule: LiveData<List<Schedule>> = scheduleDao.getAllSchedule() // 開始日時の昇順で全データを取得
+    val countDate:  LiveData<List<CountDate>> = scheduleDao.getCountDate() // 日付と日付のカウンタ数を取得
 
-    // データベースへデータを入力する
-    @WorkerThread
+    // 選択した日付の全データを取得
+    suspend fun getDaySchedule(data: String): List<Schedule>{
+        return scheduleDao.getDaySchedule(data)
+    }
+
+    // IDに一致するデータを取得
+    suspend fun getIDSchedule(id: Int): Schedule {
+        return scheduleDao.getIDSchedule(id)
+    }
+
+    // データベースへデータを入力
     suspend fun insert(schedule: Schedule) {
         scheduleDao.insert(schedule)
+    }
+
+    // データベースのデータを削除
+    suspend fun delete(schedule: Schedule) {
+        scheduleDao.delete(schedule)
+    }
+
+    // データベースのデータを変更
+    suspend fun update(schedule: Schedule){
+        scheduleDao.update(schedule)
     }
 }
