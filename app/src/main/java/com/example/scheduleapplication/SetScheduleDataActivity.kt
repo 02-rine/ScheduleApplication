8392b33815435cadd664b892eae942b09fe38eb1
r203323 <kobayashi.reni.ot@tut.jp>
2020-12-26 17:08:37 +0900

    feat: 新規作成時のViewに現在時刻の表示処理を追加

--- app/src/main/java/com/example/scheduleapplication/SetScheduleDataActivity.kt
+++ app/src/main/java/com/example/scheduleapplication/SetScheduleDataActivity.kt
@@ -13,7 +13,6 @@ import com.example.scheduleapplication.databinding.ActivitySetScheduleDataBindin
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_select_schedule_data.*
 import kotlinx.android.synthetic.main.activity_set_schedule_data.*
-import kotlinx.android.synthetic.main.activity_set_schedule_data.toolbar_text
 import kotlinx.android.synthetic.main.time_recyclerview_item.*
 import java.lang.IllegalArgumentException
 import java.text.ParseException
@@ -25,7 +24,7 @@ class SetScheduleDataActivity : AppCompatActivity(), TimePickerFragment.OnTimeSe
 
     private val scheduleViewModel: ScheduleViewModel by viewModels()
     private lateinit var binding: ActivitySetScheduleDataBinding
-    var schedule: Schedule = Schedule(0, "", "", "", "", "")
+    lateinit var schedule: Schedule
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,18 +32,29 @@ class SetScheduleDataActivity : AppCompatActivity(), TimePickerFragment.OnTimeSe
 
         // DataBindingの設定
         binding = DataBindingUtil.setContentView(this, R.layout.activity_set_schedule_data)
-        binding.schedule = schedule
+        binding.schedule = Schedule(0, "", "", "", "", "")
         binding.lifecycleOwner = this
 
-        val scheduleID = intent.getIntExtra("SCHEDULE_ID", 0) // タップされたRecyclerViewのIDを取得
-        val date: String? = intent.getStringExtra("DATE") // タップされたRecyclerViewの日付を取得
-        toolbar_text.text = date // ツールバーのテキストに日付を表示
+        val scheduleID = intent.getIntExtra("SCHEDULE_ID", 0) // タップされたtimeRecyclerViewのIDを取得
+        val date: String? = intent.getStringExtra("SCHEDULE_DATE") // タップされたtimeRecyclerViewの日付を取得
+        toolbarText.text = date // toolbarのtoolbarTextに日付を表示
 
         if(scheduleID != 0){
-            // IDが存在する場合、RecyclerViewから取得した各データをビューに表示
+            // IDが存在する場合、timeRecyclerViewから取得した各データをViewに表示
             scheduleViewModel.setIdSchedule(scheduleID) // タップされたRecyclerViewのIDに一致するデータを取得
             deleteButton.visibility = View.VISIBLE // 削除ボタンの表示
         }else{
+            // IDが存在しない場合、現在の時刻をViewに表示
+            val c = Calendar.getInstance()
+            val hour = c.get(Calendar.HOUR_OF_DAY) // 初期化する時間
+            val minute = c.get(Calendar.MINUTE) // 初期化する分
+            schedule = Schedule(0,
+                    "",
+                    "",
+                    date!!,
+                    "%1$02d:%2$02d". format(hour, minute),
+                    "%1$02d:%2$02d". format(hour, minute))
+            binding.schedule = schedule
             deleteButton.visibility = View.INVISIBLE // 削除ボタンの非表示
         }
 
@@ -52,17 +62,17 @@ class SetScheduleDataActivity : AppCompatActivity(), TimePickerFragment.OnTimeSe
             binding.schedule = it
         })
 
-        // ビューに入力されたデータの処理
+        // Viewに入力されたデータの処理
         saveButton.setOnClickListener {
             val replyIntent = Intent()
-            val title = titleEdit.text.toString()
-            val detail = detailEdit.text.toString()
-            val startTime: String = startTimeButton.text.toString()
-            val endTime: String = endTimeButton.text.toString()
+            val title = titleEdit.text.toString() // Viewからタイトルの取得
+            val detail = detailEdit.text.toString() // Viewから詳細の取得
+            val startTime: String = startTimeButton.text.toString() // Viewから開始時刻の取得
+            val endTime: String = endTimeButton.text.toString() // Viewから終了時刻の取得
             if (scheduleID == 0) {
                 // RecyclerViewに新たにデータを作成する処理
                 schedule = Schedule(0, title, detail, date!!, startTime, endTime)
-                scheduleViewModel.insert(schedule)
+                scheduleViewModel.insert(schedule) // データベースにデータを追加
                 // スナックバーの表示
                 Snackbar.make(it, "追加しました", Snackbar.LENGTH_SHORT)
                         .setAction("戻る") {
@@ -89,7 +99,7 @@ class SetScheduleDataActivity : AppCompatActivity(), TimePickerFragment.OnTimeSe
             val replyIntent = Intent()
             replyIntent.putExtra("DELETE", "delete")
             schedule = Schedule(scheduleID, "", "", "", "", "")
-            scheduleViewModel.delete(schedule)
+            scheduleViewModel.delete(schedule) // データベースのIDに一致するデータを削除
             // スナックバーの表示
             Snackbar.make(it, "削除しました", Snackbar.LENGTH_SHORT)
                     .setAction("戻る") {
@@ -108,16 +118,6 @@ class SetScheduleDataActivity : AppCompatActivity(), TimePickerFragment.OnTimeSe
         endTimeButton.setOnClickListener {
             val dialog = TimePickerFragment()
             dialog.show(supportFragmentManager, "endTime_dialog")
-        }
-    }
-
-    private fun String.toDate(pattern: String = "yyyy/MM/dd HH:mm"): Date?{
-        return try{
-            SimpleDateFormat(pattern).parse(this)
-        }catch(e: IllegalArgumentException) {
-            return null
-        }catch(e: ParseException){
-            return null
         }
     }
 
