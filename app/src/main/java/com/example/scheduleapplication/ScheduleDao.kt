a662cb8664e8529e9f0e88a5dabed3bb29f12315
r203323 <kobayashi.reni.ot@tut.jp>
2020-12-26 14:36:20 +0900

    feat: データベースのデータの取得・削除・更新クエリの追加

--- app/src/main/java/com/example/scheduleapplication/ScheduleDao.kt
+++ app/src/main/java/com/example/scheduleapplication/ScheduleDao.kt
@@ -1,9 +1,7 @@
 package com.example.scheduleapplication
 
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
+import androidx.room.*
 
 @Dao
 interface ScheduleDao {
@@ -12,11 +10,27 @@ interface ScheduleDao {
     @Insert
     fun insert(schedule: Schedule)
 
-    // schedule_table表の削除
-    @Query("DELETE FROM schedule_table")
-    fun deleteAll()
+    // データベースのデータを変更
+    @Update
+    fun update(schedule: Schedule)
 
-    // データベースの全データを取得
-    @Query("SELECT * FROM schedule_table")
+    // データベースのデータを削除
+    @Delete
+    fun delete(schedule: Schedule)
+
+    // 開始日時の昇順で全データを取得
+    @Query("SELECT * FROM schedule_table ORDER BY date ASC")
     fun getAllSchedule(): LiveData<List<Schedule>>
+
+    // 選択した日付の全データを取得
+    @Query("SELECT * FROM schedule_table WHERE date = :date ORDER BY startTime ASC")
+    fun getDaySchedule(date: String): List<Schedule>
+
+    // IDに一致するデータを取得
+    @Query("SELECT * FROM schedule_table WHERE id=:id")
+    fun getIDSchedule(id: Int): Schedule
+
+    // 日付と日付のカウンタ数を取得
+    @Query("SELECT date, COUNT(date)AS count FROM schedule_table GROUP BY date ORDER BY date")
+    fun getCountDate(): LiveData<List<CountDate>>
 }
